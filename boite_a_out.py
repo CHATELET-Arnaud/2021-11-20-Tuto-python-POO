a14925927366a8238532197e8294f44b9a092a0d
CHATELET-Arnaud <arnaud.chatelet@centraliens-marseille.fr>
2021-11-20 15:05:06 +0100

    first commit

---
+++ boite_a_out.py
@@ -0,0 +1,23 @@
+class BoiteAOutils:
+    def __init__(self):
+        self.outils = set()
+    def ajouter(self, outil):
+        self.outils.append(outil)
+    def enlever(self, outil):
+        self.outils.pop(outil)
+
+class Marteau:
+    def __init__(self):
+        self.couleur = "red"
+    def planter_clou():
+        pass
+    def retirer_clou():
+        pass
+
+class Tournevis:
+    def __init__(self):
+        self.taille = 5
+    def serrer_vis():
+        pass
+    def desserrer_vis():
+        pass
